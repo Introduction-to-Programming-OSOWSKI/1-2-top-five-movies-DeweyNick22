78dfc9537de149b159d03d5f5b3381caa09498f5
DeweyNick22 <817608@mystma.org>
2021-12-15 09:21:04 -0600

    3

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def topFiveMovies(a, b, c, d, e):
-    return a, b, c, d, e 
-topFiveMovies("Thor", "Iron Man", "Avengers", "Star Wars", "Forrest Gump")
+    return a + b + c + d + e
+topFiveMovies ("Thor" , "Iron Man" , "Avengers" , "Star Wars" , "Forrest Gump")
